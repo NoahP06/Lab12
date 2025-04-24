8cb8eb1dcdb8e730ba2bfb7b54f9cc4b7ca7e4f9
NoahP06 <noahbpetersen06@gmail.com>
2025-04-24 13:02:07 -0500

    Update television.py

--- television.py
+++ television.py
@@ -1,2 +1,10 @@
-class Television(self):
-    
+class Television():
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.status = bool(False)
+        self.muted = bool(False)
+        
