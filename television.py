1e733e2b7ceadb9a904d9ba19f53c1fd208a91b0
NoahP06 <noahbpetersen06@gmail.com>
2025-04-25 11:28:56 -0500

    test_television.py / televesion.py

--- television.py
+++ television.py
@@ -7,7 +7,7 @@ class Television:
     def __init__(self):
         self.__status = False
         self.__muted = False
-        self.__volume = 0
+        self.__volume = self.MIN_VOLUME
         self.__channel = self.MIN_CHANNEL
 
 
@@ -35,14 +35,14 @@ class Television:
     def volume_up(self):
         if self.__status:
             if self.__muted:
-                self.mute()
+                self.__muted = False
             if self.__volume != self.MAX_VOLUME:
                 self.__volume += 1
     
     def volume_down(self):
         if self.__status:
             if self.__muted:
-                self.mute()
+                self.__muted = False
             if self.__volume != self.MIN_VOLUME:
                 self.__volume -=1
 
